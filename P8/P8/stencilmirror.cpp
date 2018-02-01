ba5344b1d552d50149305458d093788326fad65d
Rain <923446353@qq.com>
2018-02-02 01:41:51 +0800

    learn about stencil buffer, but the exercise still has a bug

---
+++ P8/P8/stencilmirror.cpp
@@ -0,0 +1,362 @@
+#include "d3dUtility.h"
+
+
+//
+// Globals
+//
+
+IDirect3DDevice9* Device = 0; 
+
+const int Width  = 640;
+const int Height = 480;
+
+IDirect3DVertexBuffer9* VB = 0;
+//定义纹理
+IDirect3DTexture9* FloorTex  = 0;
+IDirect3DTexture9* WallTex   = 0;
+IDirect3DTexture9* MirrorTex = 0;
+//定义材质
+D3DMATERIAL9 FloorMtrl  = d3d::WHITE_MTRL;//白色材质，反射所有光
+D3DMATERIAL9 WallMtrl   = d3d::WHITE_MTRL;
+D3DMATERIAL9 MirrorMtrl = d3d::WHITE_MTRL;
+//定义茶壶的网格、位置和材质
+ID3DXMesh* Teapot = 0;
+D3DXVECTOR3 TeapotPosition(0.0f, 3.0f, -7.5f);
+D3DMATERIAL9 TeapotMtrl = d3d::YELLOW_MTRL;
+
+void RenderScene();
+void RenderMirror();
+
+//
+// 顶点结构
+//
+struct Vertex
+{
+	Vertex(){}
+	Vertex(float x, float y, float z, 
+		   float nx, float ny, float nz,
+		   float u, float v)
+	{
+		_x  = x;  _y  = y;  _z  = z;
+		_nx = nx; _ny = ny; _nz = nz;
+		_u  = u;  _v  = v;
+	}
+	float _x, _y, _z;
+	float _nx, _ny, _nz;
+	float _u, _v;
+
+	static const DWORD FVF;
+};
+const DWORD Vertex::FVF = D3DFVF_XYZ | D3DFVF_NORMAL | D3DFVF_TEX1;
+
+//
+// Framework Functions
+//
+bool Setup()
+{
+	//减弱墙壁对镜面光的反射
+	WallMtrl.Specular = d3d::WHITE * 0.2f;
+	//创建茶壶
+	D3DXCreateTeapot(Device, &Teapot, 0);
+	//创建墙壁、镜面、地面
+	//   |----|----|----|
+	//   |Wall|Mirr|Wall|
+	//   |    | or |    |
+	//   /--------------/
+	//  /   Floor      /
+	// /--------------/
+	Device->CreateVertexBuffer(
+		24 * sizeof(Vertex),
+		D3DUSAGE_WRITEONLY,
+		Vertex::FVF,
+		D3DPOOL_MANAGED,
+		&VB,
+		0);
+	Vertex* v = 0;
+	VB->Lock(0, 0, (void**)&v, 0);
+	//地面顶点
+	v[0] = Vertex(-7.5f, 0.0f, -10.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f);
+	v[1] = Vertex(-7.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f);
+	v[2] = Vertex(7.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f);
+
+	v[3] = Vertex(-7.5f, 0.0f, -10.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f);
+	v[4] = Vertex(7.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f);
+	v[5] = Vertex(7.5f, 0.0f, -10.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f);
+
+	//墙壁顶点
+	v[6] = Vertex(-7.5f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f);
+	v[7] = Vertex(-7.5f, 5.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f);
+	v[8] = Vertex(-2.5f, 5.0f, 0.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f);
+
+	v[9] = Vertex(-7.5f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f);
+	v[10] = Vertex(-2.5f, 5.0f, 0.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f);
+	v[11] = Vertex(-2.5f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f);
+	
+	v[12] = Vertex(2.5f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f);
+	v[13] = Vertex(2.5f, 5.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f);
+	v[14] = Vertex(7.5f, 5.0f, 0.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f);
+
+	v[15] = Vertex(2.5f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f);
+	v[16] = Vertex(7.5f, 5.0f, 0.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f);
+	v[17] = Vertex(7.5f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f);
+	//镜面在两块墙壁中间
+	v[18] = Vertex(-2.5f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f);
+	v[19] = Vertex(-2.5f, 5.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f);
+	v[20] = Vertex(2.5f, 5.0f, 0.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f);
+
+	v[21] = Vertex(-2.5f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f);
+	v[22] = Vertex(2.5f, 5.0f, 0.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f);
+	v[23] = Vertex(2.5f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f);
+
+	VB->Unlock();
+	//加载纹理，设置纹理过滤器
+	D3DXCreateTextureFromFile(Device, "checker.jpg", &FloorTex);
+	D3DXCreateTextureFromFile(Device, "brick0.jpg", &WallTex);
+	D3DXCreateTextureFromFile(Device, "ice.bmp", &MirrorTex);
+
+	Device->SetSamplerState(0, D3DSAMP_MAGFILTER, D3DTEXF_LINEAR);
+	Device->SetSamplerState(0, D3DSAMP_MINFILTER, D3DTEXF_LINEAR);
+	Device->SetSamplerState(0, D3DSAMP_MIPFILTER, D3DTEXF_LINEAR);
+	//灯光
+	D3DXVECTOR3 lightDir(0.707f, -0.707f, 0.707f);
+	D3DXCOLOR color(1.0f, 1.0f, 1.0f, 1.0f);
+	D3DLIGHT9 light = d3d::InitDirectionalLight(&lightDir, &color);
+
+	Device->SetLight(0, &light);
+	Device->LightEnable(0, true);
+
+	Device->SetRenderState(D3DRS_NORMALIZENORMALS, true);
+	Device->SetRenderState(D3DRS_SPECULARENABLE, true);//启用镜面高光
+
+	//设置摄像机
+	D3DXVECTOR3    pos(-10.0f, 3.0f, -15.0f);
+	D3DXVECTOR3 target(0.0, 0.0f, 0.0f);
+	D3DXVECTOR3     up(0.0f, 1.0f, 0.0f);
+
+	D3DXMATRIX V;
+	D3DXMatrixLookAtLH(&V, &pos, &target, &up);
+
+	Device->SetTransform(D3DTS_VIEW, &V);
+	//投影
+	D3DXMATRIX proj;
+	D3DXMatrixPerspectiveFovLH(
+		&proj,
+		D3DX_PI / 4.0f, // 45 - degree
+		(float)Width / (float)Height,
+		1.0f,
+		1000.0f);
+	Device->SetTransform(D3DTS_PROJECTION, &proj);
+	return true;
+}
+
+void Cleanup()
+{
+	//释放指针
+	d3d::Release<IDirect3DVertexBuffer9*>(VB);
+	d3d::Release<IDirect3DTexture9*>(FloorTex);
+	d3d::Release<IDirect3DTexture9*>(WallTex);
+	d3d::Release<IDirect3DTexture9*>(MirrorTex);
+	d3d::Release<ID3DXMesh*>(Teapot);
+}
+
+bool Display(float timeDelta)
+{
+	if( Device )
+	{
+		//
+		// 更新场景
+		//
+		//键盘操作和更新摄像机位置
+		static float radius = 20.0f;
+		if (::GetAsyncKeyState(VK_LEFT) & 0x8000f)
+			TeapotPosition.x -= 3.0f * timeDelta;
+
+		if (::GetAsyncKeyState(VK_RIGHT) & 0x8000f)
+			TeapotPosition.x += 3.0f * timeDelta;
+
+		if (::GetAsyncKeyState(VK_UP) & 0x8000f)
+			radius -= 2.0f * timeDelta;
+
+		if (::GetAsyncKeyState(VK_DOWN) & 0x8000f)
+			radius += 2.0f * timeDelta;
+		
+		static float angle = (3.0f * D3DX_PI) / 2.0f;
+
+		if (::GetAsyncKeyState('A') & 0x8000f)
+			angle -= 0.5f * timeDelta;
+
+		if (::GetAsyncKeyState('D') & 0x8000f)
+			angle += 0.5f * timeDelta;
+		D3DXVECTOR3 position(cosf(angle) * radius, 3.0f, sinf(angle) * radius);
+		D3DXVECTOR3 target(0.0f, 0.0f, 0.0f);
+		D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);
+		D3DXMATRIX V;
+		D3DXMatrixLookAtLH(&V, &position, &target, &up);
+		Device->SetTransform(D3DTS_VIEW, &V);
+		
+		//画场景
+		Device->Clear(0, 0,
+			D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER | D3DCLEAR_STENCIL,
+			0xff000000, 1.0f, 0L);//最后的0将模板缓存清0
+
+		Device->BeginScene();
+		//主要绘制函数
+		RenderScene();
+		RenderMirror();
+
+		Device->EndScene();
+		Device->Present(0, 0, 0, 0);
+	}
+	return true;
+}
+
+//画除了镜像以外的场景
+void RenderScene()
+{
+	//画茶壶
+	Device->SetMaterial(&TeapotMtrl);
+	Device->SetTexture(0, 0);
+	D3DXMATRIX W;
+	D3DXMatrixTranslation(&W,
+		TeapotPosition.x,
+		TeapotPosition.y,
+		TeapotPosition.z);
+	Device->SetTransform(D3DTS_WORLD, &W);
+	Teapot->DrawSubset(0);
+
+	//画地面墙壁和镜子
+	D3DXMATRIX I;
+	D3DXMatrixIdentity(&I);
+	Device->SetTransform(D3DTS_WORLD, &I);
+
+	Device->SetStreamSource(0, VB, 0, sizeof(Vertex));
+	Device->SetFVF(Vertex::FVF);
+	//画地面
+	Device->SetMaterial(&FloorMtrl);
+	Device->SetTexture(0, FloorTex);
+	Device->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 2);
+	//画墙
+	Device->SetMaterial(&WallMtrl);
+	Device->SetTexture(0, WallTex);
+	Device->DrawPrimitive(D3DPT_TRIANGLELIST, 6, 4);
+	//画镜子
+	Device->SetMaterial(&MirrorMtrl);
+	Device->SetTexture(0, MirrorTex);
+	Device->DrawPrimitive(D3DPT_TRIANGLELIST, 18, 2);
+}	
+
+void RenderMirror()
+{
+	//启用模板缓存，并对相对的绘制状态进行设置
+	Device->SetRenderState(D3DRS_STENCILENABLE, true);
+	Device->SetRenderState(D3DRS_STENCILFUNC, D3DCMP_ALWAYS);
+	Device->SetRenderState(D3DRS_STENCILREF, 0x1);
+	Device->SetRenderState(D3DRS_STENCILMASK, 0xffffffff);
+	Device->SetRenderState(D3DRS_STENCILWRITEMASK, 0xffffffff);
+	Device->SetRenderState(D3DRS_STENCILZFAIL, D3DSTENCILOP_KEEP);
+	Device->SetRenderState(D3DRS_STENCILFAIL, D3DSTENCILOP_KEEP);
+	Device->SetRenderState(D3DRS_STENCILPASS, D3DSTENCILOP_REPLACE);
+
+	// 阻止往深度缓存中进行写操作，借助融合操作并设置融合因子
+	Device->SetRenderState(D3DRS_ZWRITEENABLE, false);
+	Device->SetRenderState(D3DRS_ALPHABLENDENABLE, true);
+	Device->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_ZERO);
+	Device->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_ONE);
+
+	// 将镜子所在加入模板缓存
+	Device->SetStreamSource(0, VB, 0, sizeof(Vertex));
+	Device->SetFVF(Vertex::FVF);
+	Device->SetMaterial(&MirrorMtrl);
+	Device->SetTexture(0, MirrorTex);
+	D3DXMATRIX I;
+	D3DXMatrixIdentity(&I);
+	Device->SetTransform(D3DTS_WORLD, &I);
+	Device->DrawPrimitive(D3DPT_TRIANGLELIST, 18, 2);
+
+	// 重新开启深度缓存可写
+	Device->SetRenderState(D3DRS_ZWRITEENABLE, true);
+
+	// 重设置一些绘制状态
+	Device->SetRenderState(D3DRS_STENCILFUNC, D3DCMP_EQUAL);
+	Device->SetRenderState(D3DRS_STENCILPASS, D3DSTENCILOP_KEEP);
+
+	// 位置映射，即在镜子中的镜像的位置
+	D3DXMATRIX W, T, R;
+	D3DXPLANE plane(0.0f, 0.0f, 1.0f, 0.0f); // xy所在平面
+	D3DXMatrixReflect(&R, &plane);
+
+	D3DXMatrixTranslation(&T,
+		TeapotPosition.x,
+		TeapotPosition.y,
+		TeapotPosition.z);
+
+	W = T * R ;
+
+	// 为了能够画出镜子后的镜像茶壶，清空深度缓存，再将茶壶和镜面进行融合，设置融合因子
+	Device->Clear(0, 0, D3DCLEAR_ZBUFFER, 0, 1.0f, 0);
+	Device->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_DESTCOLOR);
+	Device->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_ZERO);
+
+	// 画出镜像茶壶
+	Device->SetTransform(D3DTS_WORLD, &W);
+	Device->SetMaterial(&TeapotMtrl);
+	Device->SetTexture(0, 0);
+
+	Device->SetRenderState(D3DRS_CULLMODE, D3DCULL_CW);
+	Teapot->DrawSubset(0);
+
+	// 恢复一些设置
+	Device->SetRenderState(D3DRS_ALPHABLENDENABLE, false);
+	Device->SetRenderState(D3DRS_STENCILENABLE, false);
+	Device->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);
+}
+
+//
+// WndProc
+//
+LRESULT CALLBACK d3d::WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch( msg )
+	{
+	case WM_DESTROY:
+		::PostQuitMessage(0);
+		break;
+		
+	case WM_KEYDOWN:
+		if( wParam == VK_ESCAPE )
+			::DestroyWindow(hwnd);
+
+		break;
+	}
+	return ::DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+//
+// WinMain
+//
+int WINAPI WinMain(HINSTANCE hinstance,
+				   HINSTANCE prevInstance, 
+				   PSTR cmdLine,
+				   int showCmd)
+{
+	if(!d3d::InitD3D(hinstance,
+		Width, Height, true, D3DDEVTYPE_HAL, &Device))
+	{
+		::MessageBox(0, "InitD3D() - FAILED", 0, 0);
+		return 0;
+	}
+		
+	if(!Setup())
+	{
+		::MessageBox(0, "Setup() - FAILED", 0, 0);
+		return 0;
+	}
+
+	d3d::EnterMsgLoop( Display );
+
+	Cleanup();
+
+	Device->Release();
+
+	return 0;
+}
